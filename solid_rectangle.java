d89200e9e8aa283e8243c7516ad38988bf75d391
Siddhant Singh <thakursiddhant9335@gmail.com>
2026-06-01 12:16:10 +0530

    Add files via upload

---
+++ solid_rectangle.java
@@ -0,0 +1,12 @@
+public class solid_rectangle {
+    public static void main(String[] args) {
+
+        int n = 3;
+        for(int i=1; i<=n; i++){
+            for(int j = 1; j<=5; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
