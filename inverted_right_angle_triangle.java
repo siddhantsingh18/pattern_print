6051ef5c3c2b5d5630b720d279b2a5a1e454344a
Siddhant Singh <thakursiddhant9335@gmail.com>
2026-06-02 10:20:25 +0530

    Add files via upload

---
+++ inverted_right_angle_triangle.java
@@ -0,0 +1,12 @@
+public class inverted_right_angle_triangle {
+    public static void main(String[] args) {
+
+        int n = 5;
+        for(int i = 1; i <=n; i++) {
+            for(int j = 1; j<=n-i+1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
