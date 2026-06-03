d63094216a00d38e527370156b672d15b1cc0498
Siddhant Singh <thakursiddhant9335@gmail.com>
2026-06-03 19:47:33 +0530

    Add files via upload

---
+++ hollow_rectangle.java
@@ -0,0 +1,25 @@
+public class hollow_rectangle {
+    public static void main(String[] args) {
+
+        int n = 4;
+        for(int i = 1; i<=n; i++){
+
+            for(int j = 1; j<=6; j++) {
+                if (i == 1 || i == n) {
+                    System.out.print("* ");
+                } else {
+                    //middle rows
+                    if (j == 1) {
+                        System.out.print("* ");
+                    } else if (j == 6) {
+                        System.out.print("* ");
+                    } else {
+                        System.out.print("  ");
+                    }
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
