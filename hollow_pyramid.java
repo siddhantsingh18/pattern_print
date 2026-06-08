93ae298d9a7269e59de6715d5344721161f8147a
Siddhant Singh <siddhant.s.9335@gmail.com>
2026-06-08 19:30:56 +0530

    Added New Pattern

---
+++ hollow_pyramid.java
@@ -0,0 +1,30 @@
+public class hollow_pyramid {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for(int i = 1; i<=n; i++) {
+
+            // part 1
+            for(int j=1; j<=n-i; j++) {
+                System.out.print("  ");
+            }
+
+            //part 2
+            if(i == 1 || i == n) {
+                for (int j = 1; j <= 2 * i - 1; j++) {
+                    System.out.print("* ");
+                }
+            }
+            else {
+                System.out.print("* ");
+                for(int j = 1; j<= 2*i-3; j++) {
+                    System.out.print("  ");
+                }
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
